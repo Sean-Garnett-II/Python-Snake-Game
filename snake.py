abccd817ca981afcc6ff81390ef91d2a949cb8d5
Sean Garnett <sean.garnett@protonmail.com>
2021-04-27 16:52:14 -0400

    added snack, collision detection, and fixed movement, Game finished

--- snake.py
+++ snake.py
@@ -3,10 +3,13 @@ import random
 import pygame
 import tkinter as tk
 from tkinter import messagebox
+import sys
 
 # side length of the square grid
-gridLength = 500
+gridLength = 200
 spacing = 20
+# number of row or column lines
+rowCol = gridLength // spacing
 displaySurface = pygame.display.set_mode((gridLength, gridLength))
 
 
@@ -30,39 +33,55 @@ class snake(object):
     turns = {}
 
     def __init__(self):
-        self.head = cube((0, 0), (1, 0))
+        self.head = cube(
+            (rowCol//2, rowCol//2), (0, -1))
         self.body.append(self.head)
 
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return False
+    def move(self, key):
 
-        key = pygame.key.get_pressed()
-
-        if key[pygame.K_LEFT]:
+        # Sets the turn location and direction in the turns hash.
+        if key[pygame.K_LEFT] and self.head.vel[0] != -1 and self.head.vel[1] != 0:
             self.turns[self.head.pos[:]] = [-1, 0]
 
-        elif key[pygame.K_RIGHT]:
+        elif key[pygame.K_RIGHT] and self.head.vel[0] != 1 and self.head.vel[1] != 0:
             self.turns[self.head.pos[:]] = [1, 0]
 
-        elif key[pygame.K_UP]:
+        elif key[pygame.K_UP] and self.head.vel[0] != 0 and self.head.vel[1] != -1:
             self.turns[self.head.pos[:]] = [0, -1]
 
-        elif key[pygame.K_DOWN]:
+        elif key[pygame.K_DOWN] and self.head.vel[0] != 0 and self.head.vel[1] != 1:
             self.turns[self.head.pos[:]] = [0, 1]
 
+        # performes the snake movement
         for i, o, in enumerate(self.body):
             p = o.pos[:]
-
             if p in self.turns:
                 o.vel = self.turns[p]
                 o.move()
                 if i == len(self.body)-1:
                     self.turns.pop(p)
             else:
-                o.move()
+                if o.pos[0] <= 0 and o.vel[0] == - 1:
+                    o.pos = (rowCol - 1, o.pos[1])
+                elif o.pos[0] >= rowCol-1 and o.vel[0] == 1:
+                    o.pos = (0, o.pos[1])
+                elif o.pos[1] <= 0 and o.vel[1] == -1:
+                    o.pos = (o.pos[0], rowCol - 1)
+                elif o.pos[1] >= rowCol-1 and o.vel[1] == 1:
+                    o.pos = (o.pos[0], 0)
+                else:
+                    o.move()
+
+    def addCube(self):
+        tmp = self.body[-1]
+        x = tmp.pos[0]
+        y = tmp.pos[1]
+        xV = tmp.vel[0]
+        yV = tmp.vel[1]
+
+        tail = cube((x - xV, y-yV), (xV, yV), (255, 0, 255))
+
+        self.body.append(tail)
 
     def draw(self, surface):
         for i, o in enumerate(self.body):
@@ -70,39 +89,70 @@ class snake(object):
 
 
 def drawGrid(surface, spacing):
-
-    for lines in range(gridLength // spacing):
+    for lines in range(rowCol):
         pygame.draw.line(surface, (255, 255, 255),
                          (0, (lines + 1) * spacing), (gridLength, (lines + 1) * spacing))
         pygame.draw.line(surface, (255, 255, 255),
                          ((lines + 1) * spacing, 0), ((lines + 1) * spacing, gridLength))
 
+
 def redrawWindow(surface):
     surface.fill((0, 0, 0))
-    s.draw(surface)
+    snack.draw(surface)
+    snake.draw(surface)
     drawGrid(displaySurface, spacing)
     pygame.display.update()
 
 
+def randSnackLoc(snake):
+    while True:
+        x = random.randrange(rowCol)
+        y = random.randrange(rowCol)
+        if len(list(filter(lambda z: z.pos == (x, y), snake.body))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
+
+
+def noCollision(snake):
+    posList = []
+
+    for x in range(len(snake.body)):
+        posList.append(snake.body[x].pos)
+
+    return len(posList) == len(set(posList))
+
+
 def main():
-    global s
-    s = snake()
-    clock = pygame.time.Clock()
+    global snake, snack
 
     pygame.init()
-
+    clock = pygame.time.Clock()
     pygame.display.set_caption('SsSsSsSsnake')
-
     drawGrid(displaySurface, spacing)
 
-    s.draw(displaySurface)
+    snake = snake()
+    snack = cube(randSnackLoc(snake), (0, 0), (0, 255, 0))
+
+    snack.draw(displaySurface)
+    snake.draw(displaySurface)
 
     while True:
+        pygame.time.delay(50)
         clock.tick(10)
-        s.move()
-        redrawWindow(displaySurface)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    pass
+        if noCollision(snake):
+            snake.move(pygame.key.get_pressed())
+            if snake.body[0].pos == snack.pos:
+                snake.addCube()
+                snack = cube(randSnackLoc(snake), (0, 0), (0, 255, 0))
+        redrawWindow(displaySurface)
 
 
 main()
